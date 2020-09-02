6b0abae5335b0ee305e509bd70c6fffa7410ae98
Richard Summer <richardforrestsummer@gmail.com>
2020-09-02 14:02:30 -0400

    Displaying current time and padded with zeros to be exactly two digits

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function updateDisplay() {
-  const date = new Date();
-  console.log(date)
-}
+const $display = document.querySelector('.clock-display');
 
-setInterval(updateDisplay, 1000);
+setInterval(setTime, 1000);
+
+function setTime() {
+  const now = new Date();
+
+  const hours = ('0' + now.getHours()).slice(-2);
+  const minutes = ('0' + now.getMinutes()).slice(-2);
+  const seconds = ('0' + now.getSeconds()).slice(-2);
+
+  $display.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
